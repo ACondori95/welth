f3aab4c68452f7a555697ec069a602d68aff6b32
Álvaro Condorí <condorialvaro95@gmail.com>
2025-07-09 15:06:50 -0300

    Bulk Delete Transactions

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {bulkDeleteTransactions} from "@/actions/accounts";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
 import {Checkbox} from "@/components/ui/checkbox";
@@ -29,6 +30,7 @@ import {
 } from "@/components/ui/table";
 import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
 import {categoryColors} from "@/data/category";
+import useFetch from "@/hooks/use-fetch";
 import {format} from "date-fns";
 import {
   ChevronDown,
@@ -41,7 +43,9 @@ import {
   X,
 } from "lucide-react";
 import {useRouter} from "next/navigation";
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
+import {BarLoader} from "react-spinners";
+import {toast} from "sonner";
 
 const RECURRING_INTERVALS = {
   DAILY: "Daily",
@@ -61,6 +65,12 @@ const TransactionTable = ({transactions}) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [typeFilter, setTypeFilter] = useState("");
   const [recurringFilter, setRecurringFilter] = useState("");
+
+  const {
+    loading: deleteLoading,
+    fn: deleteFn,
+    data: deleted,
+  } = useFetch(bulkDeleteTransactions);
 
   const filteredAndSortedTransactions = useMemo(() => {
     let result = [...transactions];
@@ -134,7 +144,23 @@ const TransactionTable = ({transactions}) => {
     );
   };
 
-  const handleBulkDelete = () => {};
+  const handleBulkDelete = async () => {
+    if (
+      !window.confirm(
+        `Are you sure you want to delete ${selectedIds.length} transactions?`
+      )
+    ) {
+      return;
+    }
+
+    deleteFn(selectedIds);
+  };
+
+  useEffect(() => {
+    if (deleted && !deleteLoading) {
+      toast.error("Transactions deleted successfully");
+    }
+  }, [deleted, deleteLoading]);
 
   const handleClearFilters = () => {
     setSearchTerm("");
@@ -145,6 +171,10 @@ const TransactionTable = ({transactions}) => {
 
   return (
     <div className='space-y-4'>
+      {deleteLoading && (
+        <BarLoader className='mt-4' width={"100%"} color='#9333ea' />
+      )}
+
       {/* Filters */}
       <div className='flex flex-col sm:flex-row gap-4'>
         <div className='relative flex-1'>
@@ -350,8 +380,7 @@ const TransactionTable = ({transactions}) => {
                         <DropdownMenuSeparator />
                         <DropdownMenuItem
                           className='text-destructive'
-                          // onClick={() => deleteFn([transaction.id])}
-                        >
+                          onClick={() => deleteFn([transaction.id])}>
                           Delete
                         </DropdownMenuItem>
                       </DropdownMenuContent>
